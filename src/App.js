ae5b43675b8cfec0c24270634bfa288334de8139
abhishek abhi <abhishekabhiabhi0@gmail.com>
2020-10-05 16:10:09 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
+const styleMenuItem = {
+  float: "left",
+  padding: "10px",
+  listStyleType: "none",
+  color: "white"
+}
+
+function aa2() {
+  console.log("krishn mani 2")
+}
+
+const aa3 = () => {
+  console.log("krishn mani 3")
+}
+
+/**
+ * onClick , onSelect ,onDrag etc.......
+ * they are action in javascript 
+ * we have to pass the function as a parameter 
+ * we dont have to call a function inside them
+ */
+
+/**
+ * function call - functionCall()
+ * function pass - functionPass
+ */
+
+/**
+ * now THIS key word is reseved to js
+ * this key word used in class componet of react 
+ * this key word used to accessing the current object of the class
+ */
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment >
+      <ul style={{ backgroundColor: "coral", overflow: "auto", margin: "0px", padding: "0px" }}>
+        <li style={styleMenuItem} onClick={() => { console.log("krishn mani") }}>Home</li>
+        <li style={styleMenuItem} onClick={aa2}>Carrer</li>
+        <li style={styleMenuItem} onClick={aa3}>Contact</li>
+        <li style={styleMenuItem} onClick={() => { console.log("krishn mani 4") }}>About</li>
+      </ul>
+    </Fragment>
   );
 }
 
